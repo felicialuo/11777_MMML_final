e8ccdcf370cc58e8f8d380a5fe67a0afd8a3231f
Felicia Luo <129685045+felicialuo@users.noreply.github.com>
2024-04-16 13:22:35 -0400

    Add eval

--- train.py
+++ train.py
@@ -16,18 +16,18 @@ warnings.filterwarnings("ignore")
 warnings.simplefilter("ignore", UserWarning)
 
 
-def train_one_epoch(train_loader, model, device, optimizer, criterion):
+def train_one_epoch(model, train_loader, device, optimizer, criterion, epoch):
     model.train()
     losses = []
     correct = 0
     total = 0
     
-    for batch in train_loader:
+    batch_idx = 0
+    for batch in tqdm(train_loader, total=train_loader.dataset.num_videos//train_loader.batch_size+1, position=0, leave=False, desc="Train Epoch {}".format(epoch)):
         label = batch["labels"].to(device)
         total += label.shape[0]
-        video_feats = videomae.get_videomae_feats(videomae_model, batch, device)
 
-        logits, feat = model(video_feats)
+        logits, feat = model(batch)
         loss = criterion(logits, label)
         loss.backward()
         optimizer.step()
@@ -42,22 +42,69 @@ def train_one_epoch(train_loader, model, device, optimizer, criterion):
 
         losses.append(loss.item())
 
-        print(f"Loss: {loss.item()}, Correct / total: {correct} / {total}")
-    return losses, correct / total
+        if batch_idx % 10 == 0:
+            tqdm.write('\tTrain Set Batch {}: Current loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+                        batch_idx, loss.item(), correct, total,
+                        100. * correct / total))
+        batch_idx += 1
+
+    average_loss = np.mean(losses)
+
+    print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        average_loss, correct, total,
+        100. * correct / total))
+    return average_loss, correct / total
 
 
-def train(model, device, train_loader, optimizer, criterion, scheduler, num_epoch):
-    model = model.to(device)
+def eval(model, test_loader, device, criterion, epoch):
+    model.eval()
     losses = []
+    correct = 0
+    with torch.no_grad():
+        for batch in tqdm(test_loader, total=test_loader.dataset.num_videos//test_loader.batch_size+1, position=0, leave=False, desc="Test Epoch {}".format(epoch)):
+            label = batch["labels"].to(device)
+            logits, feat = model(batch)
+            loss = criterion(logits, label)
+            pred = logits.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(label.view_as(pred)).sum().item()
+            losses.append(loss.item())
+
+    total = test_loader.dataset.num_videos
+    average_loss = np.mean(losses)
+    
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        average_loss, correct, total,
+        100. * correct / total))
+    return average_loss, correct / total
+
+
+
+def train(model, device, train_loader, test_seen_loader, test_unseen_loader, optimizer, criterion, scheduler, num_epoch):
+    model = model.to(device)
+    train_losses = [] # all one value per epoch
     train_accuracies = []
+    test_seen_losses = []
+    test_seen_accuracies = []
+    test_unseen_losses = []
+    test_unseen_accuracies = []
 
     for epoch in range(num_epoch):
-        print("Epoch", epoch)
-        loss, train_accuracy = train_one_epoch(train_loader, model, device, optimizer, criterion)
-        losses.extend(loss)
+        train_loss, train_accuracy = train_one_epoch(model, train_loader, device, optimizer, criterion, epoch)
+        train_losses.append(train_loss) # average loss of every epoch
         train_accuracies.append(train_accuracy)
 
+        test_seen_loss, test_seen_accuracy = eval(model, test_seen_loader, device, criterion, epoch)
+        test_seen_losses.append(test_seen_loss) # average loss of every epoch
+        test_seen_accuracies.append(test_seen_accuracy)
+
+        test_unseen_loss, test_unseen_accuracy = eval(model, test_unseen_loader, device, criterion, epoch)
+        test_unseen_losses.append(test_unseen_loss) # average loss of every epoch
+        test_unseen_accuracies.append(test_unseen_accuracy)
+
         scheduler.step()
+
+    return train_losses, train_accuracies, test_seen_losses, test_seen_accuracies, test_unseen_losses, test_unseen_accuracies
+
 
     
 
@@ -82,14 +129,16 @@ if __name__ == "__main__":
     print("datasets", train_dataset.num_videos, test_seen_dataset.num_videos, test_unseen_dataset.num_videos)
     
     train_loader, test_seen_loader, test_unseen_loader = data.get_dataloader(train_dataset, test_seen_dataset, test_unseen_dataset, 
-                                                                        image_processor, data.collate_fn, videomae_model, batch_size=16)
+                                                                        image_processor, data.collate_fn, videomae_model, batch_size=8)
     
-    model = transformerNet.TempNet()
+    model = transformerNet.TempNet(videomae_model, emb_size=768, num_class=51, device=device)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     criterion = nn.CrossEntropyLoss()
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
-    train(model, device, train_loader, optimizer, criterion, scheduler, num_epoch=5)
+    train_losses, train_accuracies, test_seen_losses, test_seen_accuracies, test_unseen_losses, test_unseen_accuracies = train(model, device, 
+                                                                                                                               train_loader, test_seen_loader, test_unseen_loader, 
+                                                                                                                               optimizer, criterion, scheduler, num_epoch=5)
 
 
 
